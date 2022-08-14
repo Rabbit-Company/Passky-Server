a1d92faa88e0127c8505a3ab2137c49e5c10bea4
Ziga <ziga.zajc007@gmail.com>
2022-08-14 22:30:56 +0200

    Add support for Tor

--- api/src/index.php
+++ api/src/index.php
@@ -14,8 +14,8 @@ if(empty($_GET['action'])){
 }
 
 header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Credentials: true');
-header('Access-Control-Max-Age: 86400');
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 86400");
 
 if($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 	if(isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
